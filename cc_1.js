00fb86ca501e8dc74a3cd479a93e98ea1eeea976
Alex Cleveland <acleveland2@usf.edu>
2024-08-31 21:06:13 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+//Task 1: Variables and Data Types
+
+let employeeName = "Tim Tom";
+const employeeID = 21;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName))
+console.log(employeeID, typeof(employeeID))
+console.log(isActive, typeof(isActive))
