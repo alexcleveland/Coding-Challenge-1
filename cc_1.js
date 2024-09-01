090fc1f94f993f15be01ecfc5ee12ba1faefd782
Alex Cleveland <acleveland2@usf.edu>
2024-08-31 21:36:22 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -4,6 +4,18 @@ let employeeName = "Tim Tom";
 const employeeID = 21;
 var isActive = true;
 
-console.log(employeeName, typeof(employeeName))
-console.log(employeeID, typeof(employeeID))
-console.log(isActive, typeof(isActive))
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
+
+
+// Task 2:Compound Data Types
+
+let products = ["Xbox", "TV","Remote"];
+const productDetails = {
+    name: stuff,
+    price: 2000.00,
+    inStock: yes,
+}
+console.log(products, productDetails);
